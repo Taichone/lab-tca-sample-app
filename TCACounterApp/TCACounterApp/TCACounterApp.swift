39f213400510a5dc4c420412a3bcc0ec265da2d5
Taichone <taichone.dev@gmail.com>
2024-06-28 14:03:10 +0900

    initial commit

---
+++ TCACounterApp/TCACounterApp/TCACounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterApp.swift
+//  TCACounterApp
+//
+//  Created by Taichi on 2024/06/28.
+//
+
+import SwiftUI
+
+@main
+struct TCACounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
